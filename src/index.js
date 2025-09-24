9fb4e0ee6ff3f50b1f3117d68e5f81677897e8ba
natua <natnaelmulealemu@gmail.com>
2025-09-24 07:52:53 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import authRoutes from "./routes/authroutes.js";
+import bookRoutes from "./routes/bookroutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
